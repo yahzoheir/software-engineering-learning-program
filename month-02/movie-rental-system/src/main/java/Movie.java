f1d3a85cf0798e84aa09d6453ebccc66693a09cd
yahzoheir <yahzoheir@gmail.com>
2026-09-23 17:38:10 +0300

    implement Movie class with validation and tests

---
+++ month-02/movie-rental-system/src/main/java/Movie.java
@@ -0,0 +1,33 @@
+public class Movie {
+    
+    private final int id;
+    private final String title;
+    private int availableCopies;
+
+    public Movie(int id, String title, int availableCopies) {
+        if (availableCopies < 0) {
+            throw new IllegalArgumentException("Available copies cannot be negative");
+        }
+
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Movie name cannot be null or whitespace only");
+        }
+
+        this.id = id;
+        this.title = title;
+        this.availableCopies = availableCopies;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getAvailableCopies() {
+        return this.availableCopies;
+    }
+
+}
--- month-02/movie-rental-system/src/test/java/MovieTest.java
+++ month-02/movie-rental-system/src/test/java/MovieTest.java
@@ -1,6 +1,50 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
 
 public class MovieTest {
-    
+
+    @Test
+    void constructorStoresMovieDetails() {
+        Movie movie = new Movie(1, "Inception", 2);
+
+        assertEquals(1, movie.getId());
+        assertEquals("Inception", movie.getTitle());
+        assertEquals(2, movie.getAvailableCopies());
+    }
+
+    @Test 
+    void constructorRejectsNegativeAvailableCopies() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Movie(1, "Inception", -1);
+    });
+    }
+
+    @Test 
+    void constructorAcceptsZeroAvailableCopies() {
+        Movie movie = new Movie(1, "Inception", 0);
+        assertEquals(0, movie.getAvailableCopies());
+    }
+
+    @Test 
+    void constructorRejectsNullTitle() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Movie(1, null , 2);
+        }); 
+    }
+
+    @Test 
+    void constructorRejectsWhiteSpaceOnlyTitle() {
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Movie(1, "   ", 1);
+        });
+    }
+
+    @Test 
+    void constructorRejectsEmptyTitle() {
+        assertThrows( IllegalArgumentException.class, () -> {
+            new Movie(1, "", 1);
+        });
+    }
+
 }
